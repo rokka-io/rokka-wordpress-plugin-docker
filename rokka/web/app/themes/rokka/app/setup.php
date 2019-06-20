5e305563c4438df40d10f2469b79e1be0db5d516
Gian Z'Graggen <gian.zgraggen@liip.ch>
2019-06-20 17:36:27 +0200

    Move image sizes to setup

--- rokka/web/app/themes/rokka/app/setup.php
+++ rokka/web/app/themes/rokka/app/setup.php
@@ -130,3 +130,30 @@ add_action('after_setup_theme', function () {
         return "<?= " . __NAMESPACE__ . "\\asset_path({$asset}); ?>";
     });
 });
+
+/**
+ * Define image sizes to test rokka stacks
+ */
+add_action( 'after_setup_theme', function () {
+    // Image size medium 375w <unlimited>h
+    update_option( 'medium_size_w', 370 );
+    update_option( 'medium_size_h', 9999 );
+    // Image size medium_large 750w <unlimited>h
+    update_option( 'medium_large_size_w', 735 );
+    update_option( 'medium_large_size_h', 9999 );
+    // Image size medium-large-to-large 1250w <unlimited>h
+    add_image_size( 'medium-large-to-large', 1250, 9999 );
+    // Image size responsive can be used to stretch image to the full with of the container.
+    add_image_size( 'responsive', 1250, 9999 );
+    // Image size large 1500w <unlimited>h
+    update_option( 'large_size_w', 1500 );
+    update_option( 'large_size_h', 9999 );
+    // Image size colmd4-image is used render different sizes attribute for images in a col-md-4 column.
+    add_image_size( 'colmd4-image', 1500, 9999 );
+    // Image size teaser 750w 375h cropped
+    add_image_size( 'teaser', 750, 375, false );
+    add_image_size( 'ratio-2-to-1-2000w', 2000, 1000, true );
+    add_image_size( 'ratio-2-to-1-1000w', 1000, 500, true );
+    add_image_size( 'ratio-2-to-1-750w', 750, 375, true );
+    add_image_size( 'ratio-2-to-1-500w', 500, 250, true );
+});
